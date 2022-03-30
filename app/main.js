6b12851db93e7ee962f4d137ea36d66241587606
minimuscle <joshthiele@live.com.au>
2022-03-30 16:56:00 +1100

    added electron

---
+++ app/main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadURL("http://localhost:3000")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+})
